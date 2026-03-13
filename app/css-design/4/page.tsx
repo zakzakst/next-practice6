8609e1f3d40c020bc94879c70b063bff5f56dc8d
zakzakst <e1433104@gmail.com>
2026-03-13 11:42:14 +0900

    fix

---
+++ app/css-design/4/page.tsx
@@ -0,0 +1,13 @@
+// import styles from "./styles.module.css";
+import "./4.css";
+import { Hero } from "./parts/Hero";
+
+const Page = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default Page;
