178452de9a1a4087081cbae4159041081a8a12f4
zakzakst <e1433104@gmail.com>
2026-03-14 14:31:42 +0900

    fix

--- app/css-design/4/page.tsx
+++ app/css-design/4/page.tsx
@@ -1,13 +1,16 @@
 // import styles from "./styles.module.css";
 import "./4.css";
+import { ProgressBar } from "./parts/ProgressBar";
 import { Header } from "./parts/Header";
 import { Hero } from "./parts/Hero";
 
 const Page = () => {
   return (
     <>
+      <ProgressBar />
       <Header />
       <Hero />
+      <div style={{ height: "100vh" }}></div>
     </>
   );
 };
