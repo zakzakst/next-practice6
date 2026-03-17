3aa88572780e888b06fa0c385320439923ff2b17
zakzakst <e1433104@gmail.com>
2026-03-17 12:59:43 +0900

    fix

---
+++ app/css-design/4/parts/CollectionSection.tsx
@@ -0,0 +1,57 @@
+export const CollectionSection = () => {
+  return (
+    <div
+      id="collection"
+      className="section section-collection container container--lg"
+    >
+      <div className="section-title">
+        <span className="section-title__sub">New Collection</span>
+        <h2 className="section-title__main">新作コレクション</h2>
+      </div>
+      <div className="item-list">
+        <a className="item" href="###">
+          <div className="item__media img-frame">
+            <img
+              src="img/item-1.jpg"
+              width="960"
+              height="640"
+              alt="セラミックの花瓶"
+            />
+          </div>
+          <div className="item__content">
+            <h3 className="item__title">Ceramic Vase - 'Suna'</h3>
+            <p className="item__text">Pottery / Japan</p>
+          </div>
+        </a>
+        <a className="item" href="###">
+          <div className="item__media img-frame">
+            <img
+              src="img/item-2.jpg"
+              width="960"
+              height="640"
+              alt="上質なレザーの小物"
+            />
+          </div>
+          <div className="item__content">
+            <h3 className="item__title">Premium Leather Goods</h3>
+            <p className="item__text">Leather Craft / Italy</p>
+          </div>
+        </a>
+        <a className="item" href="###">
+          <div className="item__media img-frame">
+            <img
+              src="img/item-3.jpg"
+              width="960"
+              height="640"
+              alt="木製の椅子"
+            />
+          </div>
+          <div className="item__content">
+            <h3 className="item__title">Oak Dining Chair</h3>
+            <p className="item__text">Furniture / Denmark</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
