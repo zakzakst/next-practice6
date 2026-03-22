1cd6814079d58ee55a881f682b5a65e9e16012c9
zakzakst <e1433104@gmail.com>
2026-03-22 19:17:05 +0900

    fix

--- app/animation-timeline/page.tsx
+++ app/animation-timeline/page.tsx
@@ -1,5 +1,7 @@
 // import { Parts } from "./parts/Parts1";
-import { Parts } from "./parts/Parts2";
+// import { Parts } from "./parts/Parts2";
+// import { Parts } from "./parts/Parts3";
+import { Parts } from "./parts/Parts4";
 
 const Page = () => {
   return (
---
+++ app/animation-timeline/parts/Parts4.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import styles from "./parts4.module.css";
+
+export const Parts = () => {
+  return <div>parts</div>;
+};
