9e42e2a310fcbf1acda295b20adf97a7dcc15e0c
zakzakst <e1433104@gmail.com>
2026-03-22 19:30:41 +0900

    fix

--- app/animation-timeline/parts/Parts4.tsx
+++ app/animation-timeline/parts/Parts4.tsx
@@ -3,5 +3,27 @@
 import styles from "./parts4.module.css";
 
 export const Parts = () => {
-  return <div>parts</div>;
+  return (
+    <>
+      <div className="h-screen" />
+
+      <section className={styles.feature}>
+        <div>
+          <img
+            className={styles.feature__image}
+            src="https://placehold.jp/1200x800.png"
+            alt=""
+          />
+        </div>
+        <div className={styles.feature__content}>
+          <h2 className={styles.feature__title}>Amazing Feature</h2>
+          <p className={styles.feature__text}>
+            This animation is synchronized using view-timeline.
+          </p>
+        </div>
+      </section>
+
+      <div className="h-screen" />
+    </>
+  );
 };
