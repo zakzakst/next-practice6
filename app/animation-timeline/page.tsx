5558aab3fcedf7bb6558616e9631d7612024222f
zakzakst <e1433104@gmail.com>
2026-03-22 18:59:57 +0900

    fix

---
+++ app/animation-timeline/page.tsx
@@ -0,0 +1,12 @@
+// import { Parts } from "./parts/Parts1";
+import { Parts } from "./parts/Parts2";
+
+const Page = () => {
+  return (
+    <div className="p-4 h-[200vh]">
+      <Parts />
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/animation-timeline/parts/Parts1.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import styles from "./parts1.module.css";
+
+export const Parts = () => {
+  return <div className={styles.box}>Scroll me</div>;
+};
