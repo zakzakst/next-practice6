73c2e0837eb631516ee02cd234b798dc57e9f1b0
zakzakst <e1433104@gmail.com>
2026-09-07 23:04:49 +0900

    fix

---
+++ app/chart-form/parts/Parts1.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+// https://chachart.net/pie
+
+export const Parts = () => {
+  return (
+    <div>
+      <input type="text" defaultValue={`項目1`} />
+      <input type="number" defaultValue="0" />
+      <button>削除</button>
+    </div>
+  );
+};
---
+++ app/chart-form/parts/_Parts.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const Parts = () => {
+  return <div>parts</div>;
+};
