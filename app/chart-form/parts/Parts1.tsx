518c70612208660f72861ec9c2feff5766e4865d
zakzakst <e1433104@gmail.com>
2026-09-08 23:49:45 +0900

    fix

--- app/chart-form/parts/Parts1.tsx
+++ app/chart-form/parts/Parts1.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 // https://chachart.net/pie
+// https://bulma.io/documentation/start/overview/
+// http://localhost:3000/chart-form
 
 export const Parts = () => {
   return (
     <div>
-      <input type="text" defaultValue={`項目1`} />
+      <input type="text" defaultValue={`項目1`} className="input is-link" />
       <input type="number" defaultValue="0" />
       <button>削除</button>
     </div>
