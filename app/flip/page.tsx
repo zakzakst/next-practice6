9f679d5c4f62ee0a29ec77c838ebb45520880f43
zakzakst <e1433104@gmail.com>
2026-09-21 17:49:37 +0900

    fix

---
+++ app/flip/page.tsx
@@ -0,0 +1,14 @@
+// http://localhost:3000/flip
+// https://ics.media/entry/240902/
+
+import { Parts } from "./parts/Parts1";
+
+const Page = () => {
+  return (
+    <div className="p-4">
+      <Parts />
+    </div>
+  );
+};
+
+export default Page;
